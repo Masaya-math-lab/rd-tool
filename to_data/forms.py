2a573c9f708437f0b49de6bf146ba1bf118c1322
田口雅也 <math.lab.opt@gmail.com>
2021-12-02 14:48:05 +0900

    url-input追加

--- to_data/forms.py
+++ to_data/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from . import variable as var
 
 class FileUpload(forms.Form):
-    file = forms.FileField(required=True, widget=forms.widgets.FileInput)
+    file = forms.FileField(required=False)
+    url = forms.URLField(required=False)
     category = forms.ChoiceField(required=True, widget=forms.RadioSelect, choices=var.CATEGORIES, initial='0')
 
 class SelectColumns(forms.Form):
