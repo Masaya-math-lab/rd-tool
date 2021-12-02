4366cdb80ee2ffec07edd76fc79ec6782565e628
田口雅也 <math.lab.opt@gmail.com>
2021-12-02 12:21:08 +0900

    first

--- to_data/forms.py
+++ to_data/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from . import variable as var
 
 class FileUpload(forms.Form):
-    # file = forms.FileField(required=True, widget=forms.widgets.FileInput)
+    file = forms.FileField(required=True, widget=forms.widgets.FileInput)
     category = forms.ChoiceField(required=True, widget=forms.RadioSelect, choices=var.CATEGORIES, initial='0')
 
 class SelectColumns(forms.Form):
